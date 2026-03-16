744a6bdeab9537790078219195a75a0b0fce9650
Kxxcelo1 <207324420+Kingsleyxelo@users.noreply.github.com>
2026-03-16 23:20:04 +0200

    Test

---
+++ data/q1_decision_tree.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+import pandas as pd
+
+x = np.array([[2, 50],[3,60],[4,70],[5,80],[6,90],[7,95]])
+y = np.array([0,0,0,1,1,1])
+
+model = DecisionTreeClassifier(random_state=32)
+model.fit(x,y)
+
+new_stud = [[4,65]]
+prediction = model.predict(new_stud)
+print(f"Prediction for the new student is: {prediction}")
