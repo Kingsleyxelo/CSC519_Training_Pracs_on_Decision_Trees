b4496b07ae40e5051e52396ba3784b2ca95db7f5
Kxxcelo1 <207324420+Kingsleyxelo@users.noreply.github.com>
2026-03-16 23:38:25 +0200

    Test

--- data/q1_decision_tree.py
+++ data/q1_decision_tree.py
@@ -1,12 +1,12 @@
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
-x = np.array([[2, 50],[3,60],[4,70],[5,80],[6,90],[7,95]])
-y = np.array([0,0,0,1,1,1])
+hour_attendence = np.array([[1,40],[2, 50],[3,60],[4,70],[5,80],[6,90],[7,95]])
+fail_pass = np.array([0,0,0,1,1,1,1])
 
-model = DecisionTreeClassifier(random_state=32)
-model.fit(x,y)
+model = DecisionTreeClassifier(random_state=56)
+model.fit(hour_attendence,fail_pass)
 
-new_stud = [[5,85]]
+new_stud = [[7,75]]
 prediction = model.predict(new_stud)
 print(f"Prediction for the new student is: {'Pass' if prediction[0] == 1 else 'Fail'}")
